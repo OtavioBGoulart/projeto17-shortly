1de958e7e0e24a8df814a100416ef361f438e3fe
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-21 14:01:42 -0300

    refeat: index.js configs

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import usersRouters from "./routes/usersRoutes.js";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(usersRouters);
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server running in port ${port}`));
