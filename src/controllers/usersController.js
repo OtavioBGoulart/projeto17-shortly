bfd9a3387ee38cef380dcc6596e679a6ccbb8b28
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-21 20:08:31 -0300

    feat: url post controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
 import { connectionDB } from "../database/db.js";
 
 
@@ -31,4 +32,25 @@ export async function signIn(req, res) {
       )
 
       res.status(200).send({ token });
+}
+
+export async function shortenUrl(req, res) {
+    const { url } = req.body;
+    const  { user } = req.user;
+
+    try {
+        const shortUrl = nanoid(10);
+
+        await connectionDB.query(`
+            INSERT INTO urls (url, short_url, visit_count, user_id)
+            VALUES ($1, $2, $3, $4)
+        ;`, [url, shortUrl, 0, user.user_id])
+
+        res.status(201).send({ shortUrl });
+
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
 }
