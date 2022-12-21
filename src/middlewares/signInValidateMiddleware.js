4fbaafffd0d10f709b90c710bf59edf199bbf17e
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-21 10:01:51 -0300

    feat: signIN validate

---
+++ src/middlewares/signInValidateMiddleware.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcrypt";
+
+export async function singInValidate(req, res, next)  {
+
+    const { email, password } = req.body;
+
+    try { 
+        const userExist = await connectionDB.query(`
+        SELECT * FROM users WHERE email = $1;
+        `, [email])
+        if (userExist.rowCount === 0) return res.sendStatus(401);
+
+        const passwordCompare = bcrypt.compare(password, userExist.password);
+        if (!passwordCompare) return res.sendStatus(401);
+
+        req.singInData = userExist.rows[0];
+
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
+    next();
+}
