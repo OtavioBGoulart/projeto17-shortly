6b05d61380a456de6f47d58acb0cc27006319094
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-22 02:38:12 -0300

    refact: insert user on jwt config

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { nanoid } from "nanoid";
 import { connectionDB } from "../database/db.js";
 
 
@@ -21,10 +20,10 @@ export async function signUp(req, res) {
 
 export async function signIn(req, res) {
     console.log(req.signInData)
-    const { id, email } = req.signInData;
+    const { id, name } = req.signInData;
 
     const token = jwt.sign(
-        { user_id: id, email },
+        { user_id: id, name },
         process.env.TOKEN_KEY,
         {
           expiresIn: 60 * 60 * 12,
@@ -34,45 +33,9 @@ export async function signIn(req, res) {
       res.status(200).send({ token });
 }
 
-export async function shortenUrl(req, res) {
-    const { url } = req.body;
-    const  user  = req.user;
-    console.log(user, url);
+export async function myUrls(req, res) {
 
-    try {
-        const shortUrl = nanoid(6);
 
-        await connectionDB.query(`
-            INSERT INTO urls (url, short_url, visit_count, user_id)
-            VALUES ($1, $2, $3, $4)
-        ;`, [url, shortUrl, 0, user.user_id])
-
-        res.status(201).send({ shortUrl });
-
-    } catch(error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
 }
 
-export async function getUrlById(req, res) {
 
-    const { id } = req.params;
-    console.log(id)
-    try {
-        const urls = await connectionDB.query(`
-            SELECT * FROM urls
-            WHERE id = $1
-        ;`, [id])
-
-        if (urls.rowCount === 0) return res.sendStatus(404);
-
-        console.log(urls.rows[0]);
-        const { short_url, url} = urls.rows[0];
-        res.status(200).send({ id, shortUrl: short_url, url});
-
-    } catch(error) {
-        console.log(error);
-        return res.sendStatus(500);
-    }
-}
