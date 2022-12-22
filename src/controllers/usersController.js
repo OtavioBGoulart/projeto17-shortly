ab61e930e98426273400137ce710b06a135bd7d4
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-22 03:01:51 -0300

    feat: my urls controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -20,10 +20,10 @@ export async function signUp(req, res) {
 
 export async function signIn(req, res) {
     console.log(req.signInData)
-    const { id, name } = req.signInData;
+    const { id, username } = req.signInData;
 
     const token = jwt.sign(
-        { user_id: id, name },
+        { user_id: id, username },
         process.env.TOKEN_KEY,
         {
           expiresIn: 60 * 60 * 12,
@@ -34,7 +34,23 @@ export async function signIn(req, res) {
 }
 
 export async function myUrls(req, res) {
+    const user = req.user;
 
+    try {
+        const myUrls = await connectionDB.query(`
+            SELECT id, short_url, url, visit_count FROM urls
+            WHERE user_id = $1
+        ;`, [user.user_id])
+
+        console.log(myUrls.rows);
+        // const shortenedUrls = query.rows
+
+        // res.status(200).send({id: user_id, name: user_username, shortenedUrls });
+        res.status(200)
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 
 }
 
