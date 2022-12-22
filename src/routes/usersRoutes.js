182e32eec1580e36c404e98bc1389db2be043dcb
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-22 15:03:07 -0300

    feat: ranking router

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { myUrls, signIn, signUp } from "../controllers/usersController.js";
+import { getRanking, myUrls, signIn, signUp } from "../controllers/usersController.js";
 import { signUpValidation } from "../middlewares/signupValidateMiddleware.js";
 import { singInValidation } from "../middlewares/signInValidateMiddleware.js";
 import { authorization } from "../middlewares/authMiddleware.js";
@@ -10,5 +10,6 @@ const router = Router();
 router.post("/sign-up", signUpValidation, signUp);
 router.post("/sign-in", singInValidation, signIn);
 router.get("/users/me", authorization, validateUser, myUrls);
+router.get("/ranking", getRanking);
 
 export default router;
